4b6fa6ddc5e43fc14537ed51ba8c27854c85db48
Anibal Rivero <riveroleonel@gmail.com>
2023-05-10 14:59:44 -0300

    modificaciones

---
+++ script.js
@@ -0,0 +1,8 @@
+const cuerpoDePagina = document.querySelector("body");
+
+function modificacionBody() {
+    cuerpoDePagina.style.color = "black";
+    cuerpoDePagina.style.backgroundColor = "white";
+    cuerpoDePagina.style.fontSize = "35px";
+    cuerpoDePagina.style.fontFamily = "arial";
+}
